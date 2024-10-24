9af64f6c5e2c0c4a303e7e8078fb3c1a031adec5
AG0G <147729717+AG0G@users.noreply.github.com>
2024-10-24 16:33:48 +0530

    Update coupons.py

--- coupons.py
+++ coupons.py
@@ -126,7 +126,7 @@ class Scrapper:
     async def coursevania(self, page) -> list:
         cv_links = []
         async with self.session(headers=self.head) as ass:
-            content = self.__fetch_html("https://coursevania.com/courses/")
+            content = self.__fetch_html("https://coursevania.com/courses/"),"html5lib")
             soup = self.parse_html(content)
             
             nonce = json.loads(
